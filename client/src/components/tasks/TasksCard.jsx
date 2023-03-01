4499ca31020045f047d4181788d150fd9c6342a8
Kevin Picado <112902865+kpv22@users.noreply.github.com>
2023-03-01 00:49:04 -0600

    fix bug in the updatelist form

--- client/src/components/tasks/TasksCard.jsx
+++ client/src/components/tasks/TasksCard.jsx
@@ -7,21 +7,24 @@ import {
   AiOutlineEdit,
   AiOutlineClose,
 } from "react-icons/ai";
+import { BsCheck2All } from "react-icons/bs";
 import { FiMoreHorizontal } from "react-icons/fi";
 import { useParams } from "react-router-dom";
 import { toast } from "react-toastify";
 
 export function TasksCard({ task }) {
+  const params = useParams();
   const [deleteTask] = useMutation(DELETE_TASK, {
     refetchQueries: ["getProject"],
   });
   const [editing, setEditing] = useState(false);
   const [title, setTitle] = useState(task.title);
+  const [showOptions, setShowOptions] = useState(false);
+  const [showMore, setShowMore] = useState(false);
   const [updatedTitle, setUpdatedTitle] = useState(title); // new state to hold updated title
   const [updateTask] = useMutation(UPDATE_TASK, {
     refetchQueries: ["getProject"],
   });
-  const params = useParams();
 
   const handleEditClick = () => {
     setEditing(true);
@@ -31,6 +34,7 @@ export function TasksCard({ task }) {
     setEditing(false);
     setTitle(task.title);
     setUpdatedTitle(title); // reset updated title state as well
+    setShowMore(false);
   };
 
   const handleSaveClick = () => {
@@ -39,6 +43,7 @@ export function TasksCard({ task }) {
     });
     setTitle(updatedTitle); // set current title to new title
     setEditing(false);
+    setShowMore(false);
     toast.success("Task updated successfully", {
       position: "top-left",
       autoClose: 3000,
@@ -55,8 +60,16 @@ export function TasksCard({ task }) {
     setUpdatedTitle(e.target.innerText); // update updated title state
   };
 
+  const handleShowOptionsClick = () => {
+    setShowOptions(!showOptions);
+  };
+
   return (
-    <div className="bg-zinc-900 px-5 py-3 mb-2 flex justify-between rounded-lg">
+    <div
+      className={`bg-zinc-900 ${
+        editing ? "bg-zinc-800 border border-white border-opacity-20" : ""
+      } px-5 py-3 mb-2 flex justify-between rounded-lg`}
+    >
       <div
         className={`outline-none text-sm ${editing ? "bg-transparent" : ""}`}
         contentEditable={editing}
@@ -68,22 +81,47 @@ export function TasksCard({ task }) {
       </div>
 
       <div>
-        {editing ? null : (
-          <button className="mx-1 shadow-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-400">
-            <AiOutlineCheck />
-          </button>
-        )}
-        <button className="mx-1 shadow-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-400">
-          <FiMoreHorizontal />
-        </button>
+        {!editing && (
+          <>
+            <button className="mx-1 shadow-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-400">
+              <AiOutlineCheck />
+            </button>
+            <button
+              className="mx-1 shadow-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-400"
+              onClick={() => setShowMore(!showMore)}
+            >
+              <FiMoreHorizontal />
+            </button>
 
-        {editing ? (
+            {showMore && (
+              <>
+                <button
+                  className="mx-1 shadow-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-400"
+                  onClick={handleEditClick}
+                >
+                  <AiOutlineEdit />
+                </button>
+                <button
+                  className="mx-1 shadow-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-400"
+                  onClick={() => {
+                    deleteTask({
+                      variables: { id: task._id },
+                    });
+                  }}
+                >
+                  <AiOutlineDelete />
+                </button>
+              </>
+            )}
+          </>
+        )}
+        {editing && (
           <>
             <button
               className="mx-1 shadow-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-400"
               onClick={handleSaveClick}
             >
-              <AiOutlineCheck />
+              <BsCheck2All />
             </button>
             <button
               className="mx-1 shadow-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-400"
@@ -92,25 +130,7 @@ export function TasksCard({ task }) {
               <AiOutlineClose />
             </button>
           </>
-        ) : (
-          <button
-            className="mx-1 shadow-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-400"
-            onClick={handleEditClick}
-          >
-            <AiOutlineEdit />
-          </button>
         )}
-
-        <button
-          className="mx-1 shadow-md hover:bg-gray-600 focus:outline-none focus:ring-2 focus:ring-gray-400"
-          onClick={() => {
-            deleteTask({
-              variables: { id: task._id },
-            });
-          }}
-        >
-          <AiOutlineDelete />
-        </button>
       </div>
     </div>
   );
