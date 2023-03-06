f7d186cffb3a828a7c28a483513d087ac78752de
Kevin Picado <112902865+kpv22@users.noreply.github.com>
2023-03-06 17:04:31 -0600

    .

--- server/index.js
+++ server/index.js
@@ -2,6 +2,9 @@ import { startApolloServer } from "./app.js";
 import { typeDefs } from "./graphql/typeDefs.js";
 import { resolvers } from "./graphql/resolvers.js";
 import { connectDB } from "./db.js";
+import dotenv from "dotenv";
 
+dotenv.config();
+const port = process.env.BACKEND_PORT || 4000;
 connectDB();
 startApolloServer(typeDefs, resolvers);
